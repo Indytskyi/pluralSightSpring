af2a2420d9dc6665975efbbecd746161fe478cdb
Indytskyi <ind.artem2003@gmail.com>
2022-07-14 10:45:04 +0300

    before Thymeleaf

---
+++ src/main/java/com/pluralsight/indytskyi/conference/controller/RegistrationController.java
@@ -0,0 +1,27 @@
+package com.pluralsight.indytskyi.conference.controller;
+
+import com.pluralsight.indytskyi.conference.model.Registration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+
+    @GetMapping("registration")
+    public String getRegistration(@ModelAttribute("registration")Registration registration) {
+
+        return "registration";
+    }
+
+    @PostMapping("registration")
+    public String postRegistration(@ModelAttribute("registration")Registration registration) {
+        System.out.println("Registration: " + registration.getName());
+        return "redirect:registration";
+    }
+
+
+}
